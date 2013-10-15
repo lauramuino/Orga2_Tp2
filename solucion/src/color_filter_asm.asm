62dcde4d251874b361ef746212a454eeacf663c8
Damian Castro <ltdicai@gmail.com>
2013-10-15 20:15:58 -0300

    Ahora esta mas nuevo

--- solucion/src/color_filter_asm.asm
+++ solucion/src/color_filter_asm.asm
@@ -9,6 +9,16 @@ section .data
 align 16
 todos_ceros:	dq 0
 				dq 0
+mask_gris:		dd 0xFFFFFFFF, 0x0
+				dd 0x0, 0xFFFFFFFF
+un_tercio:		dq 0.3333333333
+				dq 0.3333333333
+mask_broadcast:	db 0x00,0x00,0x00,0x01,0x01,0x01,0x02,0x02
+				db 0x02,0x03,0x03,0x03,0x00,0x00,0x00,0x00
+mask_comp:		db 0x00,0x00,0x00,0x04,0x04,0x04,0x08,0x08
+				db 0x08,0x0C,0x0C,0x0C,0x0C,0x0D,0x0E,0x0F
+solo_4_bytes:	db 0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF
+				db 0xFF,0xFF,0xFF,0xFF,0x00,0x00,0x00,0x00
 
 section .bss
 
@@ -43,7 +53,7 @@ color_filter_asm:
 	push R12
 	push R13
 	push R14
-	;sub RSP, 8
+	sub RSP, 32
 	
 .cargar_color_deseado:
 	;Cargo color_deseado en memoria
@@ -53,23 +63,24 @@ color_filter_asm:
 	mov RCX, 2 ;Cargo un 5
 	mov R12, color_deseado ;R12 = mm128 reservado
 .c_d_ciclo:
-	mov RBX, RDX ;RBX = rc
-	mov byte [R12], BL ;R12[pixel + c_red]
+	;RDX = rc/R13 = gc/R8 = bc
+	;mov RBX, R8 ;RBX = bc
+	mov byte [R12], R8B ;R12[pixel + c_blue]
 	inc R12 ;R12++
 	mov byte [R12], 0
 	inc R12 ;R12++
-	mov RBX, R13 ;RBX = gc
-	mov byte [R12], BL ;R12[pixel + c_green]
+	;mov RBX, R13 ;RBX = gc
+	mov byte [R12], R13B ;R12[pixel + c_green]
 	inc R12 ;R12++
 	mov byte [R12], 0
 	inc R12 ;R12++
-	mov RBX, R8 ;RBX = bc
-	mov byte [R12], BL ;R12[pixel + c_blue]
+	;mov RBX, RDX ;RBX = rc
+	mov byte [R12], DL ;R12[pixel + c_red]
 	inc R12 ;R12++
 	mov byte [R12], 0
 	inc R12
 	loop .c_d_ciclo
-	mov byte [R12], 0 ;Un byte vacio de borde
+	;mov byte [R12], 0 ;Un byte vacio de borde
 	
 	
 .calcular_tam_imagen:
@@ -85,81 +96,243 @@ color_filter_asm:
 	mov EBX, 3 ;Cargo 3 en EBX
 	mul EBX ;EDX:EAX = width*height*3
 	mov R14, RDX ;Cargo parte alta en R14
-	shl R14, 32 ;Shifteo R14
+	shl R14, 32 ;Shifteo a izq R14
 	mov R14, RAX ;Cargo parte baja en R14
 	;R14 = width*height*3 = CANT_ELEMENTOS.
-
+	
+	add RDI,  210000
+	add RSI,  210000
 .inicio:
 	cmp R14, 0
-	je .fin
+	jl .fin
 .levantar_pixeles:
+	;Muevo a XMM0 4 pixeles + 4 byte de basura
 	pxor XMM0, XMM0 ;Vacio XMM0
-	;Muevo a XMM0 5 pixeles + 1 byte de basura
+	pxor XMM1, XMM1
+	pxor XMM2, XMM2
+	cmp R14, 12 ;Si es un caso borde
+	jne .no_borde
+	sub RDI, 4 ;Retrocedo el cursor
 	movdqu XMM0, [RDI]
-	pxor XMM1, XMM1 ;Vacio XMM1
+	psrldq XMM0, 4
+	jmp .continuo ;Proceso como siempre
+.no_borde:
+	movdqu XMM0, [RDI]
+.continuo:
 	movdqu XMM1, XMM0 ;Copio XMM0 en XMM1
-.antes:
-	punpcklbw XMM1, [todos_ceros] ;Aumento la precision de byte a word
-	psubusb XMM1, [color_deseado] ;Resto (rc,gc,bc) a todos los valores de XMM0 
-	;Tengo en XMM1 = (word)(canal - canal_c)
-	pmullw XMM1, XMM1 ;Elevo al cuadrado cada valor.
-	punpcklwd XMM1, [todos_ceros] ;Aumento la precision de word a dword
-	pxor XMM2, XMM2 ;Vacio XMM2
+	movdqu XMM2, XMM0 ;Copio XMM0 en XMM2
+.a_word:
+	;movdqu XMM15, [todos_ceros]
+	pxor XMM15, XMM15
+	punpcklbw XMM1, XMM15 ;Convierto a word y guardo en XMM1
+	pslldq XMM2, 2 ;Shifteo para acomodar datos
+	punpckhbw XMM2, XMM15 ;Convierto a word y guardo en XMM2
+	movdqu XMM3, XMM1 ;Copio XMM1 en XMM3
+	movdqu XMM4, XMM2 ;Copio XMM2 en XMM4
+
+.calcular_gris:
+	movdqu XMM14, [mask_gris] ;Cargo mascara
+	
+	;Calcular gris para pixeles en XMM3
+	movdqu XMM5, XMM3 ;copio XMM3 en XMM5
+	psrldq XMM5, 2
+	paddw XMM3, XMM5
+	psrldq XMM5, 2
+	paddw XMM3, XMM5 ;Consigo sumar r+g+b
+	punpcklwd XMM3, XMM15 ;XMM3 = (dword)r+g+b 2do pixel|x|x|(dword)r+g+b 1er pixel
+	pand XMM3, XMM14 ;XMM3 = (dword)r+g+b 2do pixel|0|0|(dword)r+g+b 1er pixel
+	movdqu XMM5, XMM3
+	psrldq XMM5, 8
+	paddd XMM3, XMM5 ;XMM3 = x|0|(dword)r+g+b 2do pixel|(dword)r+g+b 1er pixel
+	
+	;Calcular gris para pixeles en XMM4
+	movdqu XMM5, XMM4
+	psrldq XMM5, 2
+	paddw XMM4, XMM5
+	psrldq XMM5, 2
+	paddw XMM4, XMM5
+	punpcklwd XMM4, XMM15 ;XMM4 = (dword)r+g+b 4to pixel|x|x|(dword)r+g+b 3er pixel
+	pand XMM4, XMM14 ;XMM4 = (dword)r+g+b 4to pixel|0|0|(dword)r+g+b 3er pixel
+	movdqu XMM5, XMM4
+	psrldq XMM5, 8
+	paddd XMM4, XMM5 ;XMM4 = x|0|(dword)r+g+b 4to pixel|(dword)r+g+b 3er pixel
+	
+	
+	;Convertir a double pixeles en XMM3
+	cvtdq2pd XMM3, XMM3 ; XMM3 = (double)2do|(double)1er
+	
+	;Convertir a double pixeles en XMM4
+	cvtdq2pd XMM4, XMM4 ; XMM4 = (double)4to|(double)3er
+	
+	;Divido por 3 los pixeles en XMM3
+	mulpd XMM3, [un_tercio]
+	
+	;Divido por 3 los pixeles en XMM4
+	mulpd XMM4, [un_tercio]
+	
+	;Convierto a dword(con redondeo normal) los pixeles de XMM3
+	cvtpd2dq XMM3, XMM3 
+	
+	;Convierto a dword(con redondeo normal) los pixeles de XMM4
+	cvtpd2dq XMM4, XMM4
+	
+	;Acumulo pixeles en XMM3
+	pslldq XMM4, 8
+	paddd XMM3, XMM4 ; XMM3 = (r+g+b)/3 4to|(r+g+b)/3 3er |(r+g+b)/3 2do|(r+g+b)/3 1er 
+	
+	;Convierto de dword a word 
+	packssdw XMM3, XMM3 ;XMM3 = (qword)x|(r+g+b)/3 4to|(r+g+b)/3 3er |(r+g+b)/3 2do|(r+g+b)/3 1er 
+	
+	;Convierto de word a byte
+	packuswb XMM3, XMM3 ;XMM3 = (qword)x|(dword)x|(r+g+b)/3 4to|(r+g+b)/3 3er |(r+g+b)/3 2do|(r+g+b)/3 1er
+	
+	;Copio el mismo resultado a todos los canales
+	pshufb XMM3, [mask_broadcast]
+	movdqu XMM11, XMM3
+	;XMM11 tiene los valores reproducidos para llegar a todos los pixeles
+	
+	
+.calcular_dist:
+	movdqu XMM14, [color_deseado]
+	
+	;Resto (rc,gc,bc) a todos los valores de XMM1
+	psubusb XMM1, XMM14
+	
+	;Resto (rc,gc,bc) a todos los valores de XMM2
+	psubusb XMM2, XMM14
+	
+	;Tengo en XMM1 = (word)rgb(canal - canal_c) de los 2 primeros pixeles
+	;Tengo en XMM2 = (word)rgb(canal - canal_c) de los 2 ultimos pixeles
+	
+	;Elevo al cuadrado cada valor de XMM1
+	pmullw XMM1, XMM1 
+	
+	;Elevo al cuadrado cada valor de XMM2
+	pmullw XMM2, XMM2 
+	
+	;Copio cosas
+	movdqu XMM3, XMM1 ;Copio XMM1 en XMM3
+	movdqu XMM4, XMM2 ;Copio XMM2 en XMM4
+	
+	;Convierto los valores de XMM1 a dword
+	punpcklwd XMM1, XMM15 ;XMM1 = (dword) rgb primer pixel
+	
+	;Convierto los valores de XMM3 a dword
+	psrldq XMM3, 6
+	punpcklwd XMM3, XMM15 ;XMM3 = (dword) rgb segundo pixel
+	
+	;Convierto los valores de XMM2 a dword
+	punpcklwd XMM2, XMM15 ;XMM2 = (dword) rgb tercer pixel
+	
+	;Convierto los valores de XMM4 a dword
+	psrldq XMM4, 6
+	punpcklwd XMM4, XMM15 ;XMM4 = (dword) rgb cuarto pixel
+	
+	;Sumo r+g+b para XMM1 y convierto a double
+	movdqu XMM5, XMM1
+	psrldq XMM5, 4
+	paddd XMM1, XMM5
+	psrldq XMM5, 4
+	paddd XMM1, XMM5
+	cvtdq2pd XMM1, XMM1 ;(double)r+g+b en primer qword 
+	
+	;Sumo r+g+b para XMM3 y convierto a double
+	movdqu XMM5, XMM3
+	psrldq XMM5, 4
+	paddd XMM3, XMM5
+	psrldq XMM5, 4
+	paddd XMM3, XMM5
+	cvtdq2pd XMM3, XMM3 ;(double)r+g+b en primer qword
+	
+	;Sumo r+g+b para XMM2 y convierto a double
+	movdqu XMM5, XMM2
+	psrldq XMM5, 4
+	paddd XMM2, XMM5
+	psrldq XMM5, 4
+	paddd XMM2, XMM5
+	cvtdq2pd XMM2, XMM2 ;(double)r+g+b en primer qword
+	
+	;Sumo r+g+b para XMM4 y convierto a double
+	movdqu XMM5, XMM4
+	psrldq XMM5, 4
+	paddd XMM4, XMM5
+	psrldq XMM5, 4
+	paddd XMM4, XMM5
+	cvtdq2pd XMM4, XMM4 ;(double)r+g+b en primer qword
+	
+	;Mezclo XMM1 y XMM3 en XMM1
+	shufpd XMM1, XMM3, 0 ;XMM1 = (qword)r+g+b 2do pixel | (qword)r+g+b 1er pixel 
+	
+	;Mezclo XMM2 y XMM4 en XMM2
+	shufpd XMM2, XMM4, 0 ;XMM2 = (qword)r+g+b 4to pixel | (qword)r+g+b 3er pixel 
+	
+	;Hago raiz cuadrada de XMM1
+	sqrtpd XMM1, XMM1 
+	
+	;Hago raiz cuadrada de XMM2
+	sqrtpd XMM2, XMM2
+	
+	;Preparo para redondear hacia arriba
+	xor RAX, RAX ;Limpio RAX
+	stmxcsr dword [RSP + 8] ;[RSP+8]=MXCSR
+	stmxcsr dword [RSP + 16] ;[RSP+16]=MXCSR
+	mov EAX, [RSP+8] ;EAX=MXCSR
+	or EAX, 0x00004000 ;EAX=MXCSR con redondeo hacia arriba
+	mov [RSP+8], EAX ;[RSP+8] = MXCSR modificado
+	ldmxcsr dword [RSP+8] ;Modifico efectivamente MXCSR
+	
+	;Convierto, con ceil(), XMM1 a dword
+	cvtpd2dq XMM1, XMM1 ; XMM1 = (qword) 0 | (dword)2do pixel | (dword) 1er pixel
+	
+	;Convierto, con ceil(), XMM2 a dword
+	cvtpd2dq XMM2, XMM2 ; XMM2 = (qword) 0 | (dword)4to pixel | (dword) 3er pixel
+	
+	;Reestablezco redondeo normal
+	ldmxcsr dword [RSP+16] ;Reestablezco MXCSR
+	
+	;Combino datos en XMM1
+	pslldq XMM2, 8
+	paddd XMM1, XMM2 ;XMM1 = (dword)4to pixel | (dword)3er pixel | (dword)2do pixel | (dword)1er pixel
+	
+.comparar_dist:
+	
+	;Cargo threshold en XMM13
+	pxor XMM2, XMM2 ;Limpio XMM2
 	movdqu XMM2, XMM1 ;Copio XMM1 en XMM2
-	xor RCX, RCX
-	mov RCX, 2
-.suma_ciclo:
-	psrldq XMM2, 4
-	paddd XMM1, XMM2
-	loop .suma_ciclo
-.tengo_casi_todo:
-	cvtdq2pd XMM1, XMM1
-	sqrtpd XMM1, XMM1
-	cvtsd2si RBX, XMM1
+	xor RBX, RBX ;Limpio RBX
 	xor RAX, RAX
-	mov RAX, R9
-	cmp EBX, EAX
-	jg .es_mayor
-	movd EBX, XMM0
-	mov [RSI], BL ;DL = r
-	inc RSI
-	shr EBX, 8
-	mov [RSI], BL
-	inc RSI
-	shr EBX, 8
-	mov [RSI], BL
-	inc RSI
-	sub R14, 3
-	add RDI, 3
-	jmp .inicio	
-.es_mayor:
-	xor RBX, RBX
-	xor RAX, RAX
-	xor RDX, RDX
-	movd EBX, XMM0
-	mov DL, BL ;DL = r
-	add RAX, RDX ;RAX = r
-	shr EBX, 8
-	mov DL, BL ;DL = g
-	add RAX, RDX ;RAX = r + g
-	shr EBX, 8
-	mov DL, BL ;DL = b
-	add RAX, RDX ;RAX = r + g + b
-	xor RDX, RDX
-	xor RBX, RBX
-	mov RBX, 3
-	div RBX
-	mov [RSI], AL
-	inc RSI
-	mov [RSI], AL
-	inc RSI
-	mov [RSI], AL
-	inc RSI
-	sub R14, 3
-	add RDI, 3
-	jmp .inicio
+	mov EBX, R9D
+	mov EAX, R9D
+	shl RAX, 32
+	add RBX, RAX
+	movq XMM13, RBX ;XMM13 = 0 | 0 | threshold | threshold
+	movdqu XMM12, XMM13
+	pslldq XMM12, 8
+	paddd XMM13, XMM12 ;XMM13 = threshold | threshold | threshold | threshold
+	
+	;Realizo comparacion
+	pcmpgtd XMM2, XMM13 ;XMM2 = Si dist > threshold
+	
+	;"Desempaqueto" el resultado de la comparacion
+	pshufb XMM2, [mask_comp]
+	pand XMM2, [solo_4_bytes]
+	;Hay FF sobre los bytes a cambiar y 00 sobre los otros(y 0 sobre los que nisiquiera modifico)
+	pand XMM11, XMM2 ;Me quedo de XMM1(grises) los valores que dieron mayor al threshold
+	pandn XMM2, XMM0 
+	;XMM2 tiene los valores modificados de los pixeles a cambiar. Hay que sumarle los que no hay que cambiar
+	paddb XMM2, XMM11
+	
+	;Guardo resultado en dst*
+	movdqu [RSI], XMM2
+	
+	;Avanzo las posiciones de memoria
+	add RSI, 12
+	add RDI, 12
+	sub R14, 12
+	jmp .inicio 
+
 .fin:
-	;add RSP, 8
+	add RSP, 32
 	pop R14
 	pop R13
 	pop R12
--- solucion/src/color_filter_c.c
+++ solucion/src/color_filter_c.c
@@ -1,19 +1,31 @@
 #include <math.h>
+#include <stdio.h>
 
 int calcularDistancia(unsigned char* pixel,unsigned char rc,unsigned char gc,unsigned char bc)
 {
 	int red, green, blue, res;
-	red = pixel[0];
+	long acum;
+	int rc_i, gc_i, bc_i;
+	double aux;
+	red = pixel[2];
 	green = pixel[1];
-	blue = pixel[2];
-	red = red - rc;
+	blue = pixel[0];
+	rc_i = rc;
+	gc_i = gc;
+	bc_i = bc;
+	red = red - rc_i;
+	//if (red < 0) printf("Rojo negativo %d\n",red);
 	red = red*red;
-	green = green - gc;
+	green = green - gc_i;
+	//if (green < 0) printf("Verde negativo %d\n",green);
 	green = green*green;
-	blue = blue - bc;
+	blue = blue - bc_i;
+	//if (blue < 0) printf("Azul negativo %d\n",blue);
 	blue = blue*blue;
-	res = red + green + blue;
-	res = sqrt(res);
+	acum = red + green + blue;
+	aux = acum;
+	aux = sqrt(aux);
+	res =  ceil(aux);
 	return res;
 }
 
@@ -23,31 +35,65 @@ void color_filter_c(unsigned char *src,
                     unsigned char gc,
                     unsigned char bc,
                     int threshold,
-                    int width,
-                    int height)
+                    int height,
+                    int width)
 {
-	unsigned char pixel[3];
-	int tam_imagen = width*height;
-	int pixel_actual = 0;
-	int canal_actual = 0;
-	int distancia, gris;
-	while(pixel_actual < tam_imagen){
-		for(canal_actual = 0; canal_actual < 3; canal_actual++){
-			pixel[canal_actual] = src[pixel_actual*3 + canal_actual];
+	int noFiltrar = 1==0;
+	if(noFiltrar){
+		int tam_imagen = width*height;
+		int pixel_actual = 0;
+		while(pixel_actual < tam_imagen){
+			dst[pixel_actual*3] = src[pixel_actual*3];
+			dst[pixel_actual*3+1] = src[pixel_actual*3+1];
+			dst[pixel_actual*3+2] = src[pixel_actual*3+2];
+			pixel_actual++;
 		}
-		distancia = calcularDistancia(pixel,rc,gc,bc);
-		if(distancia > threshold){
-			gris = pixel[0] + pixel[1] + pixel[2];
-			gris = gris/3;
-			for(canal_actual = 0; canal_actual < 3; canal_actual++){
-				dst[pixel_actual*3 + canal_actual] = gris;
+	}else{
+		double aux;
+		unsigned char pixel[3];
+		int tam_imagen = width*height; 
+		long pixel_actual = 0;
+		//long pixel_actual = 230*width+ (width-4);
+		long indice_actual;
+		int canal_actual = 0;
+		int distancia, gris,p_x,p_y;
+		while(pixel_actual < tam_imagen){
+			indice_actual = pixel_actual*3;
+			pixel[0] = src[indice_actual];
+			pixel[1] = src[indice_actual+1];
+			pixel[2] = src[indice_actual+2];
+			//Pixel = [B,G,R]
+			//printf("Voy por %li\n",indice_actual);
+			//p_x = pixel_actual%width;
+			//p_y = pixel_actual/width;
+			//printf("El pixel (%i,%i)\n",p_x,p_y);
+			//printf("El pixel es: (%u,%u,%u)\n",(unsigned int)pixel[0],(unsigned int)pixel[1],(unsigned int)pixel[2]);
+			//printf("El pixel es: (%u,%u,%u)\n",(unsigned int)pixel[0],(unsigned int)pixel[1],(unsigned int)pixel[2]);
+// 			if(p_x == 957 && p_y == 230){
+// 				printf("El pixel (%i,%i)\n",p_x,p_y);
+// 				printf("El pixel es: (%u,%u,%u)\n",(unsigned int)pixel[0],(unsigned int)pixel[1],(unsigned int)pixel[2]);
+// 				distancia = calcularDistancia(pixel,rc,gc,bc);
+// 				//printf("El pixel es: (%u,%u,%u)\n",(unsigned int)pixel[0],(unsigned int)pixel[1],(unsigned int)pixel[2]);
+// 				printf("\tDist %d vs Threshold %d)\n",distancia,threshold);
+// 			}
+			distancia = calcularDistancia(pixel,rc,gc,bc);
+			if(distancia > threshold){
+				//printf("El pixel (%i,%i) excede el threshold\n",pixel_actual/width,pixel_actual % width);
+				//printf("\tDist %d > Threshold %d)\n",distancia,threshold);
+				gris = pixel[0] + pixel[1] + pixel[2];
+				aux = gris;
+				aux = aux/3;
+				gris = round(aux);
+				for(canal_actual = 0; canal_actual < 3; canal_actual++){
+					dst[indice_actual + canal_actual] = gris;
+				}
 			}
-		}
-		else{
-			for(canal_actual = 0; canal_actual < 3; canal_actual++){
-				dst[pixel_actual*3 + canal_actual] = src[pixel_actual*3 + canal_actual];
+			else{
+				dst[indice_actual] = pixel[0];
+				dst[indice_actual + 1] = pixel[1];
+				dst[indice_actual + 2] = pixel[2];
 			}
+			pixel_actual++;
 		}
-		pixel_actual++;
 	}
 }
